755c018d4e8932ee11bb8c1301dbdc626be77b0f
Chandra <apple@macbook-air.local>
2020-04-11 02:52:54 +0530

    Updates

---
+++ precache-manifest.451d38b836a7d24fe9ea31edf79c115c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a127ad3ef990ec8a47373861a63215b",
+    "url": "/MoviesInfo/index.html"
+  },
+  {
+    "revision": "07cd4f2a9a509714320c",
+    "url": "/MoviesInfo/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "94c0419c4e160ddc6e83",
+    "url": "/MoviesInfo/static/css/main.1b914824.chunk.css"
+  },
+  {
+    "revision": "07cd4f2a9a509714320c",
+    "url": "/MoviesInfo/static/js/2.588cdda9.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/MoviesInfo/static/js/2.588cdda9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94c0419c4e160ddc6e83",
+    "url": "/MoviesInfo/static/js/main.f8a751e7.chunk.js"
+  },
+  {
+    "revision": "2d3bd2ea3962ff93305c",
+    "url": "/MoviesInfo/static/js/runtime-main.b28f3b0d.js"
+  }
+]);
